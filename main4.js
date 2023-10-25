bd2111a1aa90ee47722d6bb4aea3ca1ac394efb1
b1oodsun <k.alim030205@gmail.com>
2023-10-25 13:04:45 +0600

    add files

---
+++ main4.js
@@ -0,0 +1,36 @@
+ 
+// const second = 60 * 60; 
+
+// const currentDate = new Date();
+
+// const hours = currentDate.getHours();
+// const minutes = currentDate.getMinutes();
+// const seconds = currentDate.getSeconds();
+
+// const currentTime = hours + ":" + minutes + ":" + seconds;
+// console.log("Текущее время: " + currentTime);
+// console.log("Количество секунд в часе: " + second);
+//  В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+
+// let day = prompt("введите от 1-31")
+
+// if(day > 0 && day < 11) {
+//     alert("первая")
+// } else if (day > 10 && day < 21){
+//     alert("вторая")
+// } else (day > 20 && day < 31)
+//     alert("третья")
+
+
+//  В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+
+// let month = prompt("введите от 1-12")
+
+// if(month > 0 && month < 4) {
+//     alert("зима")
+// } else if (month > 3 && month < 7){
+//     alert("весна")
+// } else if (month > 6 && month < 10){
+//     alert("лето")
+//  } else (month > 9 && month < 13)
+//     alert("осень")
